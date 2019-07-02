697ab09ba1eac037474da9cf62bd85332f83c20e
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2019-07-01 21:19:42 -0500

    Performance Tests

---
+++ First/FirstTests/PerformanceTests.swift
@@ -0,0 +1,40 @@
+//
+//  PerformanceTests.swift
+//  FirstTests
+//
+//  Created by Álvaro Ávalos Hernández on 7/1/19.
+//  Copyright © 2019 Álvaro Ávalos Hernández. All rights reserved.
+//
+
+import XCTest
+@testable import First
+
+class PerformanceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testPrimePerformance() {
+        measure {
+            _ = PrimeCalculator.calculateSync(upTo: 1_000_000)
+        }
+    }
+
+}
